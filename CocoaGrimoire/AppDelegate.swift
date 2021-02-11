74e6ae973a0027579cde0d08f53646da09f95d20
Steve Sheets <magesteve@mac.com>
2021-02-11 00:22:24 -0500

    Initial Commit

---
+++ CocoaGrimoire/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaGrimoire
+//
+//  Created by Steve Sheets on 2/11/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
